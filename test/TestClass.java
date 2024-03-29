4a1f088bfafa4008ffbc01367292d1c5081d4452
twt <1456085904@qq.com>
2024-03-29 14:05:34 +0800

    heft1.0

--- test/TestClass.java
+++ test/TestClass.java
@@ -6,19 +6,21 @@ public class TestClass {
     @Test
     public void test1(){
         DAG dag = new DAG();
-        Task t1 = new Task("a",12);
-        Task t2 = new Task("b",9);
-        Task t3 = new Task("c",7);
-        Task t4 = new Task("d",13);
+        Task t1 = new Task("a",8);
+        Task t2 = new Task("b",14);
+        Task t3 = new Task("c",9);
+        Task t4 = new Task("d",18);
         Task t5 = new Task("e",18);
-        Task t6 = new Task("f",15);
+        Task t6 = new Task("f",5);
 
-        dag.addDependency(t1,t2,8);
+        dag.addDependency(t1,t2,9);
         dag.addDependency(t1,t3,12);
-        dag.addDependency(t2,t4,12);
-        dag.addDependency(t3,t5,10);
-        dag.addDependency(t4,t6,10);
-        dag.addDependency(t5,t6,6);
+        dag.addDependency(t1,t4,14);
+        dag.addDependency(t2,t3,16);
+        dag.addDependency(t2,t5,9);
+        dag.addDependency(t3,t5,11);
+        dag.addDependency(t4,t6,7);
+        dag.addDependency(t5,t6,13);
 
         List<Task> sortedVertices = dag.topologicalSort();
         System.out.println("Topological Order:");
